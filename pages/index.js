656e6b8c701a29ec55f0adc21020d1e6b7516716
Taufik Obet <obet@happy5.co>
2024-08-08 17:59:03 +0700

    Add about link on index page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <p><Link href="/about">About</Link></p>
       </main>
 
       <Footer />
